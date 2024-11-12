f15e6368307ea1dac211a510d5790c6e190204f3
최정인 <wjddls980912@naver.com>
2024-11-12 17:04:26 +0900

    feature: ProfileRepository 구현체 구현(#4)

--- Domain/Domain/Sources/Entity/ProfileIcon.swift
+++ Domain/Domain/Sources/Entity/ProfileIcon.swift
@@ -5,7 +5,21 @@
 //  Created by 최정인 on 11/12/24.
 //
 
-public struct ProfileIcon {
+public struct ProfileIcon: Codable {
     public let emoji: String
     public let colorHex: String
+}
+
+extension ProfileIcon {
+    public static let profileIcons: [ProfileIcon] = [
+        ProfileIcon(emoji: "😇", colorHex: "FFE29A"),
+        ProfileIcon(emoji: "🥰", colorHex: "FFCCBF"),
+        ProfileIcon(emoji: "🥵", colorHex: "FFAE79"),
+        ProfileIcon(emoji: "🤢", colorHex: "B8D888"),
+        ProfileIcon(emoji: "😈", colorHex: "E2B4FF"),
+        ProfileIcon(emoji: "👻", colorHex: "DAD9D7"),
+        ProfileIcon(emoji: "🥶", colorHex: "9ACCFF"),
+        ProfileIcon(emoji: "😶‍🌫️", colorHex: "E2F5FF"),
+        ProfileIcon(emoji: "💩", colorHex: "BEA571")
+    ]
 }
