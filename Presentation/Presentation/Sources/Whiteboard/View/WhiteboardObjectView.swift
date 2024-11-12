b34e25cfc2aa75f172784ddd7adae970732b1f7e
taipaise <taipaise@gmail.com>
2024-11-12 12:12:12 +0900

    feature: 화이트보드 오브젝트 뷰 설계(#50)

---
+++ Presentation/Presentation/Sources/Whiteboard/View/WhiteboardObjectView.swift
@@ -0,0 +1,19 @@
+//
+//  WhiteboardObjectView.swift
+//  Presentation
+//
+//  Created by 이동현 on 11/12/24.
+//
+import Domain
+import UIKit
+
+class WhiteboardObjectView: UIView {
+    init(whiteboardObject: WhiteboardObject) {
+        let frame = CGRect(origin: whiteboardObject.position, size: whiteboardObject.size)
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
