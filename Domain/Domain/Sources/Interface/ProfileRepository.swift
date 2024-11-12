2f7ba777a8a538ebedb60796761418ae00e69ba6
최정인 <wjddls980912@naver.com>
2024-11-12 16:22:05 +0900

    feature: ProfileRepository 인터페이스 구현(#4)

---
+++ Domain/Domain/Sources/Interface/ProfileRepository.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileRepository.swift
+//  Domain
+//
+//  Created by 최정인 on 11/12/24.
+//
+
+public protocol ProfileRepository {
+    /// 프로필 정보를 가져옵니다.
+    /// - Returns: 가져온 프로필 정보
+    func loadProfile() -> Profile
+
+    /// 프로필 정보를 저장합니다.
+    /// - Parameter profile: 저장할 프로필 정보
+    func saveProfile(profile: Profile)
+}
