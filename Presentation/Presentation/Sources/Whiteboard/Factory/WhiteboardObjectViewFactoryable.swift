e2c6a4a9a677a112c92d25da42afc288b89f6bb3
eemdeeks <prince9088@gmail.com>
2024-11-13 23:38:37 +0900

    feature: 화이트보드 뷰 팩토리 구현

--- Presentation/Presentation/Sources/Whiteboard/Factory/WhiteboardObjectViewFactoryable.swift
+++ Presentation/Presentation/Sources/Whiteboard/Factory/WhiteboardObjectViewFactoryable.swift
@@ -5,7 +5,27 @@
 //  Created by 이동현 on 11/12/24.
 //
 import Domain
+import Foundation
 
 protocol WhiteboardObjectViewFactoryable {
     func create(with whiteboardObject: WhiteboardObject) -> WhiteboardObjectView
+}
+
+struct WhiteboardObjectViewFactory: WhiteboardObjectViewFactoryable {
+    func create(with whiteboardObject: WhiteboardObject) -> WhiteboardObjectView {
+        switch whiteboardObject {
+        case let textObject as TextObject:
+            return TextObjectView(textObject: textObject)
+        // TODO: 오브젝트 별 case 추가 예정
+        default:
+            break
+        }
+
+        return WhiteboardObjectView(
+            whiteboardObject: WhiteboardObject(
+                id: UUID(),
+                position: .zero,
+                size: .zero)
+        )
+    }
 }
