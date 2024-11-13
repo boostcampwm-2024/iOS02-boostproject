6b8f62b14348981e799e7ac684f5a89e5250f76a
taipaise <taipaise@gmail.com>
2024-11-13 21:44:29 +0900

    fix: DrawObjectUseCase 동작 방식 수정(#9)

--- Domain/Domain/Sources/UseCase/DrawObjectUseCase.swift
+++ Domain/Domain/Sources/UseCase/DrawObjectUseCase.swift
@@ -10,28 +10,58 @@ import Foundation
 final class DrawObjectUseCase: DrawObjectUseCaseInterface {
     private var drawingObject: DrawingObject?
     private let repository: WhiteboardObjectRepositoryInterface
+    private var points: [CGPoint]
+    private var origin: CGPoint?
+    private var minPoint: CGPoint?
     
     init(repository: WhiteboardObjectRepositoryInterface) {
         self.repository = repository
+        points = []
     }
     
     func startDrawing(at point: CGPoint) {
-        drawingObject = DrawingObject(
-            id: UUID(),
-            position: point,
-            size: CGSize(),
-            points: [point])
+        origin = point
     }
     
     func addPoint(point: CGPoint) {
-        drawingObject?.addPoint(point: point)
+        points.append(point)
     }
     
     func finishDrawing() -> DrawingObject? {
-        defer { drawingObject = nil }
-        guard let drawingObject else { return nil }
+        defer { reset() }
+        let (xPoints, yPoints) = points.reduce(into: ([CGFloat](), [CGFloat]())) {
+            $0.0.append($1.x)
+            $0.1.append($1.y)
+        }
         
+        guard
+            let origin,
+            let minX = xPoints.min(),
+            let maxX = xPoints.max(),
+            let minY = yPoints.min(),
+            let maxY = yPoints.max()
+        else { return nil }
+        
+        let size = CGSize(width: maxX - minX, height: maxY - minY)
+        let adjustedPoints = points.map {
+            CGPoint(x: $0.x - minX, y: $0.y - minY)
+        }
+        
+        drawingObject = DrawingObject(
+            id: UUID(),
+            position: origin,
+            size: size,
+            points: adjustedPoints)
+
+        guard let drawingObject else { return nil }
         repository.send(whiteboardObject: drawingObject)
         return drawingObject
+    }
+    
+    private func reset() {
+        drawingObject = nil
+        minPoint = nil
+        origin = nil
+        points.removeAll()
     }
 }
