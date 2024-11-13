c4c11b0f44d32bd621b2a556c8099b8dafd6ac37
taipaise <taipaise@gmail.com>
2024-11-13 22:34:24 +0900

    fix: DrawObjectUseCase를 testable한 구조로 수정(#9)

--- Domain/Domain/Sources/Interface/Repository/WhiteboardObjectRepositoryInterface.swift
+++ Domain/Domain/Sources/Interface/Repository/WhiteboardObjectRepositoryInterface.swift
@@ -4,13 +4,13 @@
 //
 //  Created by 이동현 on 11/13/24.
 //
+import Foundation
 
 public protocol WhiteboardObjectRepositoryInterface {
     /// 다른 사람이 보낸 화이트보드 오브젝트를 AsyncStream을 통해 전달하는 메서드
     /// - Returns: WhiteboardObject들이 담겨 있는 AsyncStream
     func whiteboardObjectAsyncStream() -> AsyncStream<WhiteboardObject>
-    
-    
+
     /// 다른 사람들에게 화이트보드 오브젝트를 전송하는 메서드.
     /// - Parameter whiteboardObject: 전송할 Whiteboard Object
     func send(whiteboardObject: WhiteboardObject)
