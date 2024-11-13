37711ae5bc0ab438978fff5f6d16c3a4a3b6f884
taipaise <taipaise@gmail.com>
2024-11-13 17:55:39 +0900

    feature: WhiteboardViewModel 생성(#75)

---
+++ Presentation/Presentation/Sources/Whiteboard/ViewModel/WhiteboardViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  WhiteboardViewModel.swift
+//  Presentation
+//
+//  Created by 이동현 on 11/13/24.
+//
+
+final class WhiteboardViewModel: ViewModel {
+    enum Input {}
+    struct Output {}
+    
+    func action(input: Input) {
+        
+    }
+}
