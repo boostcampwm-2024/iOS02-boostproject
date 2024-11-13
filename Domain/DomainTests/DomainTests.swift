8739264409e04f1496a3dd659d384d6f6face637
taipaise <taipaise@gmail.com>
2024-11-13 22:35:18 +0900

    feature: DrawObjectUseCase 테스트 코드 추가(#9)

--- Domain/DomainTests/DomainTests.swift
+++ Domain/DomainTests/DomainTests.swift
@@ -4,32 +4,87 @@
 //
 //  Created by 이동현 on 11/13/24.
 //
-
+import Domain
 import XCTest
 
-final class DomainTests: XCTestCase {
+final class DrawObjectUseCaseTests: XCTestCase {
+    var useCase: DrawObjectUseCaseInterface!
+    var mockRepository: WhiteboardObjectRepositoryInterface!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mockRepository = MockWhiteboardObjectRepository()
+        useCase = DrawObjectUseCase(repository: mockRepository)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockRepository = nil
+        useCase = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    // startDrawing시, 시작 좌표를 올바르게 설정하는지 확인
+    func testStartDrawingSetsOrigin() {
+        let startPoint = CGPoint(x: 100, y: 100)
+        useCase.startDrawing(at: startPoint)
+
+        XCTAssertEqual(startPoint, useCase.origin)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    // 그림을 그릴 때, points 배열에 점들을 올바르게 추가하는지 확인
+    func testAddPointToArray() {
+        let startPoint = CGPoint(x: 100, y: 100)
+        let point1 = CGPoint(x: 115, y: 115)
+        let point2 = CGPoint(x: 15, y: 30)
+        
+        useCase.startDrawing(at: startPoint)
+        useCase.addPoint(point: point1)
+        useCase.addPoint(point: point2)
+
+        XCTAssertEqual(useCase.points, [startPoint, point1, point2])
+    }
+
+    // finishDrawing이 올바르게 DrawingObject를 생성하는지 확인
+     func testFinishDrawingCreatesAndSendsDrawingObject() {
+         useCase.startDrawing(at: CGPoint(x: 10, y: 10))
+         useCase.addPoint(point: CGPoint(x: 11, y: 11))
+         useCase.addPoint(point: CGPoint(x: 12, y: 12))
+         useCase.addPoint(point: CGPoint(x: 13, y: 13))
+         
+         let drawingObject = useCase.finishDrawing()
+         
+         XCTAssertNotNil(drawingObject)
+         XCTAssertEqual(drawingObject?.position, CGPoint(x: 10, y: 10))
+         XCTAssertEqual(drawingObject?.size, CGSize(width: 3, height: 3))
+         
+         let expectedAdjustedPoints = [
+             CGPoint(x: 0, y: 0),
+             CGPoint(x: 1, y: 1),
+             CGPoint(x: 2, y: 2),
+             CGPoint(x: 3, y: 3)]
+         XCTAssertEqual(drawingObject?.points, expectedAdjustedPoints)
+     }
+    
+    // 그림 그린 후 useCase 내부 상태 초기화가 되는지 확인
+    func testResetAfterFinishDrawing() {
+        useCase.startDrawing(at: CGPoint(x: 10, y: 10))
+        useCase.addPoint(point: CGPoint(x: 110, y: 110))
+        useCase.addPoint(point: CGPoint(x: 120, y: 120))
+        _ = useCase.finishDrawing()
+
+        XCTAssertNil(useCase.origin)
+        XCTAssertEqual(useCase.points.count, 0)
+    }
+}
+
+final class MockWhiteboardObjectRepository: WhiteboardObjectRepositoryInterface {
+    private var continuation: AsyncStream<WhiteboardObject>.Continuation?
+
+    func send(whiteboardObject: WhiteboardObject) {
+        continuation?.yield(whiteboardObject)
+    }
+
+    func whiteboardObjectAsyncStream() -> AsyncStream<WhiteboardObject> {
+        return AsyncStream { continuation in
+            self.continuation = continuation
         }
     }
-
 }
