48e8e11ba79cecf373f831d0be153682ad862d87
taipaise <taipaise@gmail.com>
2024-11-13 22:34:24 +0900

    fix: DrawObjectUseCase를 testable한 구조로 수정(#9)

--- Domain/Domain/Sources/Interface/UseCase/DrawObjectUseCaseInterface.swift
+++ Domain/Domain/Sources/Interface/UseCase/DrawObjectUseCaseInterface.swift
@@ -8,16 +8,25 @@
 import Foundation
 
 public protocol DrawObjectUseCaseInterface {
+    /// 그림을 그리기 시작한 점
+    var origin: CGPoint? { get }
+
+    /// 선을 나타내는 점들의 배열
+    var points: [CGPoint] { get }
+
     /// 그리기를 시작하는 메서드
     /// - Parameter point: 시작 지점 CGPoint
     func startDrawing(at point: CGPoint)
-    
+
     /// 그림 그리는 중에 새로운 점을 추가하는 메서드
     /// - Parameter point: 추가할 점의 CGPoint
     func addPoint(point: CGPoint)
-    
-    
+
     /// 그림 그리기를 종료하는 메서드
     /// - Returns: 완성된 그림 객체 (옵셔널)
     func finishDrawing() -> DrawingObject?
+
+    /// 그림을 전송하는 메서드
+    /// - Parameter drawingObject: 전송할 그림 오브젝트
+    func send(drawingObject: DrawingObject)
 }
--- Domain/Domain/Sources/UseCase/DrawObjectUseCase.swift
+++ Domain/Domain/Sources/UseCase/DrawObjectUseCase.swift
@@ -7,33 +7,33 @@
 
 import Foundation
 
-final class DrawObjectUseCase: DrawObjectUseCaseInterface {
-    private var drawingObject: DrawingObject?
+public final class DrawObjectUseCase: DrawObjectUseCaseInterface {
     private let repository: WhiteboardObjectRepositoryInterface
-    private var points: [CGPoint]
-    private var origin: CGPoint?
-    private var minPoint: CGPoint?
-    
-    init(repository: WhiteboardObjectRepositoryInterface) {
+    public private(set) var points: [CGPoint]
+    public private(set) var origin: CGPoint?
+    private(set) var minPoint: CGPoint?
+
+    public init(repository: WhiteboardObjectRepositoryInterface) {
         self.repository = repository
         points = []
     }
-    
-    func startDrawing(at point: CGPoint) {
+
+    public func startDrawing(at point: CGPoint) {
         origin = point
+        points = [point]
     }
-    
-    func addPoint(point: CGPoint) {
+
+    public func addPoint(point: CGPoint) {
         points.append(point)
     }
-    
-    func finishDrawing() -> DrawingObject? {
+
+    public func finishDrawing() -> DrawingObject? {
         defer { reset() }
         let (xPoints, yPoints) = points.reduce(into: ([CGFloat](), [CGFloat]())) {
             $0.0.append($1.x)
             $0.1.append($1.y)
         }
-        
+
         guard
             let origin,
             let minX = xPoints.min(),
@@ -41,25 +41,27 @@ final class DrawObjectUseCase: DrawObjectUseCaseInterface {
             let minY = yPoints.min(),
             let maxY = yPoints.max()
         else { return nil }
-        
+
         let size = CGSize(width: maxX - minX, height: maxY - minY)
         let adjustedPoints = points.map {
             CGPoint(x: $0.x - minX, y: $0.y - minY)
         }
-        
-        drawingObject = DrawingObject(
+
+        let drawingObject = DrawingObject(
             id: UUID(),
             position: origin,
             size: size,
             points: adjustedPoints)
 
-        guard let drawingObject else { return nil }
-        repository.send(whiteboardObject: drawingObject)
+        send(drawingObject: drawingObject)
         return drawingObject
     }
-    
+
+    public func send(drawingObject: DrawingObject) {
+        repository.send(whiteboardObject: drawingObject)
+    }
+
     private func reset() {
-        drawingObject = nil
         minPoint = nil
         origin = nil
         points.removeAll()
