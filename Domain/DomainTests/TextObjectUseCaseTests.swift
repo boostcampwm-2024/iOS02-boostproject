b98b9119c7946b24d18fd22b0e5bdb9788043d1e
eemdeeks <prince9088@gmail.com>
2024-11-19 14:48:18 +0900

    feature: 텍스트오브젝트 추가 테스트코드(#7)

---
+++ Domain/DomainTests/TextObjectUseCaseTests.swift
@@ -0,0 +1,74 @@
+//
+//  TextObjectUseCaseTests.swift
+//  DomainTests
+//
+//  Created by 박승찬 on 11/19/24.
+//
+
+import Domain
+import XCTest
+
+final class TextObjectUseCaseTests: XCTestCase {
+    private var useCase: TextObjectUseCase!
+
+    override func setUpWithError() throws {
+        let mockCGSize = CGSize(width: 200, height: 50)
+        useCase = TextObjectUseCase(textFieldDefaultSize: mockCGSize)
+    }
+
+    override func tearDownWithError() throws {
+        useCase = nil
+    }
+
+    // addText테스트
+    // 정상적인 입력
+    func testIdealAddText() {
+        // 준비
+        let testScrollViewOffset = CGPoint(x: 0, y: 0)
+        let testViewSize = CGSize(width: 300, height: 300)
+        let expectedPosition = CGPoint(
+            x: testScrollViewOffset.x + testViewSize.width / 3,
+            y: testScrollViewOffset.y + testViewSize.height / 3)
+
+        // 실행
+        let createdTextObject = useCase.addText(scrollViewOffset: testScrollViewOffset, viewSize: testViewSize)
+
+        // 검증
+        XCTAssertEqual(createdTextObject.position, expectedPosition)
+    }
+
+    // addText테스트
+    // 비정상적인 입력(0)
+    func testStrangeZeroAddText() {
+        // 준비
+        let testScrollViewOffset = CGPoint(x: 0, y: 0)
+        let testViewSize = CGSize(width: 0, height: 0)
+        let expectedPosition = CGPoint(
+            x: testScrollViewOffset.x + testViewSize.width / 3,
+            y: testScrollViewOffset.y + testViewSize.height / 3)
+
+        // 실행
+        let createdTextObject = useCase.addText(scrollViewOffset: testScrollViewOffset, viewSize: testViewSize)
+
+        // 검증
+        XCTAssertEqual(createdTextObject.position, expectedPosition)
+    }
+
+    // addText테스트
+    // 비정상적인 입력(마이너스)
+    func testStrangeMinusAddText() {
+        // 준비
+        let testScrollViewOffset = CGPoint(x: 0, y: 0)
+        let testViewSize = CGSize(width: -300, height: -300)
+        let expectedPosition = CGPoint(
+            x: testScrollViewOffset.x + testViewSize.width / 3,
+            y: testScrollViewOffset.y + testViewSize.height / 3)
+
+        // 실행
+        let createdTextObject = useCase.addText(scrollViewOffset: testScrollViewOffset, viewSize: testViewSize)
+
+        // 검증
+        XCTAssertEqual(createdTextObject.position, expectedPosition)
+    }
+
+}
