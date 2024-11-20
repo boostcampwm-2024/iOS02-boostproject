5b26e8bc741fd725ac97523fc8bd9f427f9c3a19
taipaise <taipaise@gmail.com>
2024-11-20 15:49:04 +0900

    feature: 파일 저장을 위한 모델 정의(#96)

---
+++ DataSource/DataSource/Sources/Model/AirplaINDataType.swift
@@ -0,0 +1,15 @@
+//
+//  AirplainDataType.swift
+//  DataSource
+//
+//  Created by 이동현 on 11/20/24.
+//
+
+public enum AirplaINDataType: String, Codable {
+    case whiteboard
+    case text
+    case photo
+    case drawing
+    case game
+    case chat
+}
