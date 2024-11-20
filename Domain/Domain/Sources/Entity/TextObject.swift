26c7e5685bda1f0476d67c216007a9a65fc95eba
eemdeeks <prince9088@gmail.com>
2024-11-20 15:08:57 +0900

    feature: 오브젝트 데이터 변환을 위한 Codable채택 구현(#45, #46, #47)

--- Domain/Domain/Sources/Entity/TextObject.swift
+++ Domain/Domain/Sources/Entity/TextObject.swift
@@ -10,6 +10,8 @@ import Foundation
 public class TextObject: WhiteboardObject {
     public var text: String
 
+    private enum CodingKeys: String, CodingKey { case text }
+
     public init(
         id: UUID,
         position: CGPoint,
@@ -21,5 +23,18 @@ public class TextObject: WhiteboardObject {
             id: id,
             position: position,
             size: size)
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let text = try container.decode(String.self, forKey: .text)
+        self.text = text
+        try super.init(from: decoder)
+    }
+
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(text, forKey: .text)
+        try super.encode(to: encoder)
     }
 }
--- Domain/Domain/Sources/Entity/WhiteboardObject.swift
+++ Domain/Domain/Sources/Entity/WhiteboardObject.swift
@@ -6,7 +6,7 @@
 //
 import Foundation
 
-public class WhiteboardObject: Equatable {
+public class WhiteboardObject: Equatable, Codable {
     public let id: UUID
     public var position: CGPoint
     public var size: CGSize
@@ -23,5 +23,45 @@ public class WhiteboardObject: Equatable {
 
     public static func == (lhs: WhiteboardObject, rhs: WhiteboardObject) -> Bool {
         return lhs.id == rhs.id
+    }
+}
+
+extension CGSize: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case width
+        case height
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.width, forKey: .width)
+        try container.encode(self.height, forKey: .height)
+    }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let width = try container.decode(Double.self, forKey: .width)
+        let height = try container.decode(Double.self, forKey: .height)
+        self.init(width: width, height: height)
+    }
+}
+
+extension CGPoint: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case pointX
+        case pointY
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.x, forKey: .pointX)
+        try container.encode(self.y, forKey: .pointY)
+    }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let pointX = try container.decode(Double.self, forKey: .pointX)
+        let pointY = try container.decode(Double.self, forKey: .pointY)
+        self.init(x: pointX, y: pointY)
     }
 }
