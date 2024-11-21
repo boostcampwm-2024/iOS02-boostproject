9e022667f837f7da7ca0d2ce13df4edfdb3b12a0
taipaise <taipaise@gmail.com>
2024-11-21 13:56:32 +0900

    feature: whiteboardObjectRespository 인터페이스 설계

--- Domain/Domain/Sources/Interface/Repository/WhiteboardObjectRepositoryInterface.swift
+++ Domain/Domain/Sources/Interface/Repository/WhiteboardObjectRepositoryInterface.swift
@@ -6,11 +6,21 @@
 //
 
 public protocol WhiteboardObjectRepositoryInterface {
-    /// 다른 사람이 보낸 화이트보드 오브젝트를 AsyncStream을 통해 전달하는 메서드
-    /// - Returns: WhiteboardObject들이 담겨 있는 AsyncStream
-    func whiteboardObjectAsyncStream() -> AsyncStream<WhiteboardObject>
+    /// WhiteboardObjectRepository의 delegate
+    var delegate: WhiteboardObjectRepositoryDelegate? { get set }
 
     /// 다른 사람들에게 화이트보드 오브젝트를 전송하는 메서드.
     /// - Parameter whiteboardObject: 전송할 Whiteboard Object
     func send(whiteboardObject: WhiteboardObject)
+
+    /// 텍스트 오브젝트를 삭제합니다.
+    /// - Parameter textObject: 삭제할 Whiteboard Object
+    func delete(whiteboardObject: WhiteboardObject)
+}
+
+public protocol WhiteboardObjectRepositoryDelegate: AnyObject {
+    /// 화이트보드 오브젝트를 수신하면 실행됩니다.
+    /// - Parameters:
+    ///   - object: 수신한 화이트보드 오브젝트
+    func whiteboardObjectRepository(_ sender: WhiteboardRepositoryInterface, didReceive object: WhiteboardObject)
 }
