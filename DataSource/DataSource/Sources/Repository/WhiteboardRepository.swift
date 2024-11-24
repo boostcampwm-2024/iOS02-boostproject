9faab94bba561421ed5994660202f8c5f235bb42
Dagyeong Choi <dagyeong33@gmail.com>
2024-11-24 23:54:45 +0900

    feature: 참여자 정보 갱신을 위한 WhiteboardRepository, Interface 수정

--- DataSource/DataSource/Sources/Repository/WhiteboardRepository.swift
+++ DataSource/DataSource/Sources/Repository/WhiteboardRepository.swift
@@ -11,16 +11,21 @@ import Foundation
 public final class WhiteboardRepository: WhiteboardRepositoryInterface {
     private var nearbyNetwork: NearbyNetworkInterface
     public weak var delegate: WhiteboardRepositoryDelegate?
+    private var connections: [UUID: NetworkConnection]
+    private var participantsInfo: [String: String]
+    private let decoder = JSONDecoder()
+    private let myProfile: Profile
 
-    public init(nearbyNetworkInterface: NearbyNetworkInterface) {
+    public init(nearbyNetworkInterface: NearbyNetworkInterface, myProfile: Profile) {
         self.nearbyNetwork = nearbyNetworkInterface
+        self.participantsInfo = [:]
+        self.connections = [:]
+        self.myProfile = myProfile
         self.nearbyNetwork.connectionDelegate = self
     }
 
-    public func startPublishing(with info: [Profile]) {
-        let participantsInfo: [String: String] = ["participants": info
-            .compactMap { $0.profileIcon.emoji }
-            .joined(separator: ",")]
+    public func startPublishing() {
+        updatePublishingInfo()
         nearbyNetwork.startPublishing(with: participantsInfo)
     }
 
@@ -28,12 +33,43 @@ public final class WhiteboardRepository: WhiteboardRepositoryInterface {
         nearbyNetwork.startSearching()
     }
 
+    public func joinWhiteboard(whiteboard: Whiteboard, myProfile: Profile) throws {
+        let profileIcons = whiteboard.participantIcons
+            .map { $0.emoji }
+            .joined(separator: ",")
+
+        let participantsInfo = ["participants": profileIcons]
+
+        let connection = NetworkConnection(
+            id: whiteboard.id,
+            name: whiteboard.name,
+            info: participantsInfo)
+
+        let context = RequestedContext(participant: myProfile.profileIcon.emoji)
+
+        try nearbyNetwork.joinConnection(connection: connection, context: context)
+    }
+
     public func stopSearching() {
         nearbyNetwork.stopSearching()
     }
 
-    public func joinWhiteboard(whiteboard: Whiteboard) throws {
-        // TODO: - ..
+    private func updatePublishingInfo() {
+        var newIconList: [String] = []
+        newIconList.append(myProfile.profileIcon.emoji)
+
+        for connection in connections.values {
+            guard
+                let info = connection.info,
+                let participant = info["participants"],
+                !participant.isEmpty
+            else { continue }
+            newIconList.append(participant)
+        }
+
+        let newInfo = newIconList.joined(separator: ",")
+        self.participantsInfo["participants"] = newInfo
+        self.participantsInfo["host"] = myProfile.nickname
     }
 }
 
@@ -64,7 +100,70 @@ extension WhiteboardRepository: NearbyNetworkConnectionDelegate {
         delegate?.whiteboardRepository(self, didFind: foundWhiteboards)
     }
 
+    public func nearbyNetwork(_ sender: any NearbyNetworkInterface, didLost connection: NetworkConnection) {
+        let lostedWhiteboard = Whiteboard(
+            id: connection.id,
+            name: "",
+            participantIcons: []
+        )
+        delegate?.whiteboardRepository(self, didLost: lostedWhiteboard.id)
+    }
+
     public func nearbyNetworkCannotConnect(_ sender: any NearbyNetworkInterface) {
         // TODO: -
+    }
+
+    public func nearbyNetwork(
+        _ sender: any NearbyNetworkInterface,
+        didConnect connection: NetworkConnection,
+        with info: [String: String]
+    ) {
+        // TODO: -
+    }
+
+    public func nearbyNetwork(
+        _ sender: any NearbyNetworkInterface,
+        didConnect connection: NetworkConnection,
+        with context: Data?,
+        isHost: Bool
+    ) {
+        if !isHost { return }
+
+        do {
+            guard
+                let context = context,
+                let prevInfo = self.participantsInfo["participants"]
+            else { return }
+
+            let decodedContext = try JSONDecoder().decode(RequestedContext.self, from: context)
+            let invitationInfo = decodedContext.participant
+            let currentInfo = prevInfo + "," + invitationInfo
+            let requestedInfo = ["participants": invitationInfo]
+
+            connections[connection.id] = NetworkConnection(id: connection.id, name: "", info: requestedInfo)
+            updatePublishingInfo()
+            nearbyNetwork.startPublishing(with: self.participantsInfo)
+        } catch {
+            // TODO: - 에러 처리
+        }
+    }
+
+    public func nearbyNetwork(
+        _ sender: any NearbyNetworkInterface,
+        didDisconnect connection: NetworkConnection,
+        isHost: Bool
+    ) {
+        if !isHost { return }
+        guard
+            let prevInfo = self.participantsInfo["participants"],
+            let lostConnection = connections[connection.id],
+            let lostInfo = lostConnection.info,
+            let lostIcon = lostInfo["participants"]
+        else { return }
+
+
+        connections[connection.id] = nil
+        updatePublishingInfo()
+        nearbyNetwork.startPublishing(with: self.participantsInfo)
     }
 }
