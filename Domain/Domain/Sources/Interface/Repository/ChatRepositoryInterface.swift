0570b673bbdc0868b96178a74f5fcbff6a34b3a9
eemdeeks <prince9088@gmail.com>
2024-11-24 17:45:25 +0900

    feature: 채팅 송수신 레포지터리 설계(#110)

---
+++ Domain/Domain/Sources/Interface/Repository/ChatRepositoryInterface.swift
@@ -0,0 +1,23 @@
+//
+//  ChatRepositoryInterface.swift
+//  Domain
+//
+//  Created by 박승찬 on 11/24/24.
+//
+
+public protocol ChatRepositoryInterface {
+    /// ChatRepository의 delegate
+    var delegate: ChatRepositoryDelegate? { get set }
+
+    /// 주변 사람에게 채팅을 보내는 메소드
+    /// - Parameter message: 전달할 메시지
+    func send(message: String) async
+}
+
+public protocol ChatRepositoryDelegate: AnyObject {
+    /// 채팅을 수신하면 실행 됩니다.
+    /// - Parameters:
+    ///   - sender: delegate 객체
+    ///   - chatMessage: 수신받은 메시지
+    func chatRepository(_ sender: ChatRepositoryInterface, didReceive chatMessage: ChatMessage)
+}
