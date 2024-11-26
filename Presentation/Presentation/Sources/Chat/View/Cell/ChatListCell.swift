de5f4dcf816abcdea75fdabe8de42b4ef148c45c
eemdeeks <prince9088@gmail.com>
2024-11-26 11:13:18 +0900

    feature: chatListCell 구현(#57)

---
+++ Presentation/Presentation/Sources/Chat/View/Cell/ChatListCell.swift
@@ -0,0 +1,36 @@
+//
+//  ChatListCell.swift
+//  Presentation
+//
+//  Created by 박승찬 on 11/26/24.
+//
+
+import Domain
+import UIKit
+
+class ChatListCell: UICollectionViewListCell {
+    private var chatMessage: ChatMessage?
+
+    func update(with newMessage: ChatMessage) {
+        guard chatMessage != newMessage else { return }
+        chatMessage = newMessage
+        setNeedsUpdateConfiguration()
+    }
+
+    override var configurationState: UICellConfigurationState {
+        var state = super.configurationState
+        state.chatMessage = self.chatMessage
+        return state
+    }
+}
+
+fileprivate extension UIConfigurationStateCustomKey {
+    static let chatMessage = UIConfigurationStateCustomKey("chatMessage")
+}
+
+extension UICellConfigurationState {
+    var chatMessage: ChatMessage? {
+        get { return self[.chatMessage] as? ChatMessage }
+        set { self[.chatMessage] = newValue }
+    }
+}
