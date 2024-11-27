c4d114d5c58ee7460df10b0d2727743089f0e699
최정인 <wjddls980912@naver.com>
2024-11-27 14:43:21 +0900

    feature: WordleGuideView UI 구현(#22)

---
+++ Presentation/Presentation/Sources/Wordle/View/WordleGuideView.swift
@@ -0,0 +1,35 @@
+//
+//  WordleGuideView.swift
+//  Presentation
+//
+//  Created by 최정인 on 11/27/24.
+//
+
+import SwiftUI
+
+struct WordleGuideView: View {
+    @Binding var isShowingGuideView: Bool
+
+    var body: some View {
+        VStack {
+            Spacer()
+            ZStack(alignment: .topTrailing) {
+                Image(.wordleGuide)
+                    .resizable()
+                    .scaledToFit()
+
+                Button {
+                    isShowingGuideView.toggle()
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundStyle(.black)
+                }
+                .padding(horizontalMargin)
+            }
+            .padding(horizontalMargin)
+            Spacer()
+        }
+        .background(Color.airplainBlack.opacity(0.4))
+        .ignoresSafeArea()
+    }
+}
