04bddb2b8bf2feda6880c8c07704ca3214f07221
eemdeeks <prince9088@gmail.com>
2025-01-06 23:17:28 +0900

    feature: 클래스로 LWW레지스터 수정(#149)

--- Domain/Domain/Sources/Model/LWWRegister.swift
+++ Domain/Domain/Sources/Model/LWWRegister.swift
@@ -5,16 +5,41 @@
 //  Created by 박승찬 on 1/6/25.
 //
 
-actor LWWRegister {
-    private var whiteboardObject: WhiteboardObject
+import Foundation
+
+public class LWWRegister {
+    private(set) var whiteboardObject: WhiteboardObject
     private var timestamp: Timestamp
+    private var serialQueue: DispatchQueue
 
     init(whiteboardObject: WhiteboardObject, timestamp: Timestamp) {
         self.whiteboardObject = whiteboardObject
         self.timestamp = timestamp
+        serialQueue = DispatchQueue(label: "serial")
     }
 
-    func merge(with updatedObject: WhiteboardObject, at updatedTimestamp: Timestamp) {
-        if timestamp < updatedTimestamp { whiteboardObject = updatedObject }
+    func merge(register: LWWRegister) {
+        serialQueue.async {
+            if self.timestamp < register.timestamp {
+                self.whiteboardObject = register.whiteboardObject
+                self.timestamp = register.timestamp
+            }
+        }
+    }
+}
+
+extension LWWRegister: Hashable {
+    public static func == (lhs: LWWRegister, rhs: LWWRegister) -> Bool {
+        lhs.whiteboardObject == rhs.whiteboardObject
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(whiteboardObject)
+    }
+}
+
+extension LWWRegister: Comparable {
+    public static func < (lhs: LWWRegister, rhs: LWWRegister) -> Bool {
+        lhs.timestamp < rhs.timestamp
     }
 }
