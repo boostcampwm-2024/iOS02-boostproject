938c65fcbdc7d0dd37100644ce56578c72168111
eemdeeks <prince9088@gmail.com>
2025-01-08 21:48:46 +0900

    feature: LWWRegister 테스트(#149)

--- Domain/DomainTests/LWWRegisterTests.swift
+++ Domain/DomainTests/LWWRegisterTests.swift
@@ -5,31 +5,130 @@
 //  Created by 박승찬 on 1/8/25.
 //
 
+import Domain
 import XCTest
 
 final class LWWRegisterTests: XCTestCase {
+    private var register: LWWRegister!
+    private var defaultTimestamp: Timestamp!
+    private var defaultDate: Date!
+    private var defaultObject: WhiteboardObject!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        defaultObject = TextObject(
+            id: UUID(),
+            centerPosition: CGPoint(x: 0, y: 0),
+            size: CGSize(width: 100, height: 100),
+            text: "default")
+        defaultDate = Date()
+        defaultTimestamp = Timestamp(updatedAt: defaultDate, updatedBy: UUID())
+        register = LWWRegister(whiteboardObject: defaultObject, timestamp: defaultTimestamp)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        register = nil
+        defaultTimestamp = nil
+        defaultDate = nil
+        defaultObject = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    // Timestamp가 같을 때
+    func testMergeWhenEqualTimestmap() {
+        // 준비
+        let textObject = TextObject(
+            id: UUID(),
+            centerPosition: CGPoint(x: 50, y: 50),
+            size: CGSize(width: 200, height: 200),
+            text: "equal")
+        let mockRegister = LWWRegister(whiteboardObject: textObject, timestamp: defaultTimestamp)
+
+        // 실행
+        let sut = register.merge(register: mockRegister)
+
+        // 검증
+        XCTAssertEqual(sut, register)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // 새로 들어온 updatedAt이 더 빠를 때
+    func testMergeWhenIncomingTimestampIsEarlier() {
+        // 준비
+        let earlierDate = defaultDate.addingTimeInterval(-10)
+        let earlierTimestamp = Timestamp(updatedAt: earlierDate, updatedBy: UUID())
+        let textObject = TextObject(
+            id: UUID(),
+            centerPosition: CGPoint(x: 50, y: 50),
+            size: CGSize(width: 200, height: 200),
+            text: "incoming")
+        let mockRegister = LWWRegister(whiteboardObject: textObject, timestamp: earlierTimestamp)
+
+        // 실행
+        let sut = register.merge(register: mockRegister)
+
+        // 검증
+        XCTAssertEqual(sut, register)
+    }
+
+    // updatedAt은 같지만 새로 들어온 UUID가 작을 때
+    func testMergeWhenIncomingTimestampHasSmallerUUID() {
+        // 준비
+        guard let smallerUUID = UUID(uuidString: "00000000-0000-0000-0000-000000000000") else {
+            XCTFail("Test UUID생성 실패")
+            return
         }
+        let smallerTimestamp = Timestamp(updatedAt: defaultDate, updatedBy: smallerUUID)
+        let textObject = TextObject(
+            id: UUID(),
+            centerPosition: CGPoint(x: 50, y: 50),
+            size: CGSize(width: 200, height: 200),
+            text: "incoming")
+        let mockRegister = LWWRegister(whiteboardObject: textObject, timestamp: smallerTimestamp)
+
+        // 실행
+        let sut = register.merge(register: mockRegister)
+
+        // 검증
+        XCTAssertEqual(sut, register)
     }
 
+    // 새로 들어온 updatedAt이 더 느릴 때
+    func testMergeWhenIncomingTimestampIsLater() {
+        // 준비
+        let laterDate = defaultDate.addingTimeInterval(10)
+        let laterTimestamp = Timestamp(updatedAt: laterDate, updatedBy: UUID())
+        let textObject = TextObject(
+            id: UUID(),
+            centerPosition: CGPoint(x: 50, y: 50),
+            size: CGSize(width: 200, height: 200),
+            text: "incoming")
+        let mockRegister = LWWRegister(whiteboardObject: textObject, timestamp: laterTimestamp)
+
+        // 실행
+        let sut = register.merge(register: mockRegister)
+
+        // 검증
+        XCTAssertEqual(sut, mockRegister)
+    }
+
+    // updatedAt은 같지만 새로 들어온 UUID가 클 때
+    func testMergeWhenIncomingTimestampHasLargerUUID() {
+        // 준비
+        guard let largerUUID = UUID(uuidString: "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF") else {
+            XCTFail("Test UUID생성 실패")
+            return
+        }
+        let largerTimestamp = Timestamp(updatedAt: defaultDate, updatedBy: largerUUID)
+        let textObject = TextObject(
+            id: UUID(),
+            centerPosition: CGPoint(x: 50, y: 50),
+            size: CGSize(width: 200, height: 200),
+            text: "incoming")
+        let mockRegister = LWWRegister(whiteboardObject: textObject, timestamp: largerTimestamp)
+
+        // 실행
+        let sut = register.merge(register: mockRegister)
+
+        // 검증
+        XCTAssertEqual(sut, mockRegister)
+    }
 }
